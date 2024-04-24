a816c2da254557e40f48323c33d641a1c473f7f4
jorranMLGN <jorranhoukes1@gmail.com>
2024-04-24 11:48:39 +0200

    +Init

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { trpc } from "@/server/client";
+  import {Button} from "@nextui-org/button";
 
 export default function Home() {
   const users = trpc.users.get.useQuery();
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {JSON.stringify(users.data)}
+      <Button>Click me</Button>
     </main>
   );
 }
